c7686d5b196f3126a083efafa9c3bcada6450807
freedom <957420317@qq.com>
2023-12-16 13:36:47 +0800

    删除初始化按钮

--- web/src/view/login/index.vue
+++ web/src/view/login/index.vue
@@ -84,15 +84,6 @@
                   @click="submitForm"
                 >登 录</el-button>
               </el-form-item>
-              <el-form-item class="mb-6">
-                <el-button
-                  class="shadow shadow-blue-600 h-11 w-full"
-                  type="primary"
-                  size="large"
-                  @click="checkInit"
-                >前往初始化</el-button>
-
-              </el-form-item>
             </el-form>
           </div>
         </div>
@@ -240,21 +231,4 @@ const submitForm = () => {
     }
   })
 }
-
-// 跳转初始化
-const checkInit = async() => {
-  const res = await checkDB()
-  if (res.code === 0) {
-    if (res.data?.needInit) {
-      userStore.NeedInit()
-      router.push({ name: 'Init' })
-    } else {
-      ElMessage({
-        type: 'info',
-        message: '已配置数据库信息，无法初始化',
-      })
-    }
-  }
-}
-
 </script>
